44da60b94f0eaa260e9e5b99c9ce876b1123bbec
shuishihan <11583895+shuishihan@users.noreply.github.com>
2024-12-29 13:51:15 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -30,6 +30,8 @@ CFG_FILE="./package/base-files/files/bin/config_generate"
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+#修改雅典娜和亚瑟内核大小为12M
+sed -i 's/6144k/12288k/g' $GITHUB_WORKSPACE/wrt/target/linux/qualcommax/image/ipq60xx.mk
 
 #配置文件修改
 echo "CONFIG_PACKAGE_luci=y" >> ./.config
